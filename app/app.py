329527db437fa8f50765db535fcd41231b9db25d
EC2 Default User <ec2-user@ip-172-31-25-46.ec2.internal>
2021-02-04 20:11:01 +0000

    added random artist and song info

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import requests
 import os
 import base64
 import datetime
+import random
 from dotenv import load_dotenv, find_dotenv
 
 #do a lookup for a token
@@ -19,7 +20,7 @@ token_headers = {
 }
 # request for authentication
 r = requests.post(token_url, data = token_data, headers=token_headers)
-print(r.json())
+#print(r.json())
 
 #check for valid request
 valid_request = r.status_code in range(200, 299)
@@ -31,28 +32,32 @@ if valid_request:
     expires_in = token_response_data['expires_in']
     expires = now + datetime.timedelta(seconds=expires_in)
     did_expire = expires < now
-    print(expires)
+    #print(expires)
     
 #artist test
-artist_id = ["0blbVefuxOGltDBa00dspv"] #LiSA
+artist_id = ["0blbVefuxOGltDBa00dspv", "7BzEKSgHp2yrNC6w5NkFhQ", "7ucOhItVkxNqunNLo8AkzN"] #LiSA, Goosehouse, and FripSide
 Base_URL = f"https://api.spotify.com"
 version = f"v1"
-resource_type = "albums"
+resource_type = "top-tracks?"
 market = "market=ES"
+rand = random.randint(0, 2)
 #endpoint = f"{Base_URL}/{version}/artists/{artist_id[0]}/{resource_type}?offset=0&limit=10&include_groups=single&{market}"
-#endpoint = f"https://api.spotify.com/v1/artists/0blbVefuxOGltDBa00dspv/top-tracks?market=ES"
-endpoint = f"https://api.spotify.com/v1/browse/new-releases?country=ES&offset=0&limit=10"
+endpoint = f"{Base_URL}/{version}/artists/{artist_id[rand]}/{resource_type}{market}"
 headers = {
     "Authorization": f"Bearer {access_token}"
 }
 
 #send request for LISA albums
-#request_album = requests.get(endpoint, headers=headers)
-#lisa_albums = request_album.json()
-new_releases = requests.get(endpoint, headers=headers)
-new_releases = new_releases.json()
-#print(lisa_albums)
+request_tracks = requests.get(endpoint, headers=headers)
+tracks = request_tracks.json()
+#print(tracks)
 
 #print 10 tracks
-for i in range(0,10):
-    print(new_releases["albums"].get("items")[i]["name"])
+rand2 = random.randint(0, 9)
+track_name = (tracks["tracks"][rand2]["name"])
+track_preview = tracks["tracks"][rand2]["preview_url"]
+artist_name = tracks["tracks"][rand2]["artists"][0].get("name")
+
+print(track_name)
+print(artist_name)
+print(track_preview)
